81d33157d589dd380e082c44f4122e01b030179e
Zippy OFC <72061397+Zippy01@users.noreply.github.com>
2024-08-06 03:29:14 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+-- Please dont try to steal my work, BUT feel free to buy or obtain more free work from me at https://zippy-productions.com
+author 'Zippy OFC'
+description 'Re-enable base GTA trains in FiveM'
+version '1.0.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
